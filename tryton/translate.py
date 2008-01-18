5e08ff51fcf9a9ba59f0dcca48b61ad4331bac0d
Cédric Krier <ced@b2ck.com>
2008-01-19 00:00:17 +0100

    Fix setlocale on win32

--- tryton/translate.py
+++ tryton/translate.py
@@ -9,6 +9,74 @@ import gtk
 
 _ = gettext.gettext
 
+_LOCALE2WIN32 = {
+    'af_ZA': 'Afrikaans_South Africa',
+    'sq_AL': 'Albanian_Albania',
+    'ar_SA': 'Arabic_Saudi Arabia',
+    'eu_ES': 'Basque_Spain',
+    'be_BY': 'Belarusian_Belarus',
+    'bs_BA': 'Serbian (Latin)',
+    'bg_BG': 'Bulgarian_Bulgaria',
+    'ca_ES': 'Catalan_Spain',
+    'hr_HR': 'Croatian_Croatia',
+    'zh_CN': 'Chinese_China',
+    'zh_TW': 'Chinese_Taiwan',
+    'cs_CZ': 'Czech_Czech Republic',
+    'da_DK': 'Danish_Denmark',
+    'nl_NL': 'Dutch_Netherlands',
+    'et_EE': 'Estonian_Estonia',
+    'fa_IR': 'Farsi_Iran',
+    'ph_PH': 'Filipino_Philippines',
+    'fi_FI': 'Finnish_Finland',
+    'fr_FR': 'French_France',
+    'fr_BE': 'French_France',
+    'fr_CH': 'French_France',
+    'fr_CA': 'French_France',
+    'ga': 'Scottish Gaelic',
+    'gl_ES': 'Galician_Spain',
+    'ka_GE': 'Georgian_Georgia',
+    'de_DE': 'German_Germany',
+    'el_GR': 'Greek_Greece',
+    'gu': 'Gujarati_India',
+    'he_IL': 'Hebrew_Israel',
+    'hi_IN': 'Hindi',
+    'hu': 'Hungarian_Hungary',
+    'is_IS': 'Icelandic_Iceland',
+    'id_ID': 'Indonesian_indonesia',
+    'it_IT': 'Italian_Italy',
+    'ja_JP': 'Japanese_Japan',
+    'kn_IN': 'Kannada',
+    'km_KH': 'Khmer',
+    'ko_KR': 'Korean_Korea',
+    'lo_LA': 'Lao_Laos',
+    'lt_LT': 'Lithuanian_Lithuania',
+    'lat': 'Latvian_Latvia',
+    'ml_IN': 'Malayalam_India',
+    'id_ID': 'Indonesian_indonesia',
+    'mi_NZ': 'Maori',
+    'mn': 'Cyrillic_Mongolian',
+    'no_NO': 'Norwegian_Norway',
+    'nn_NO': 'Norwegian-Nynorsk_Norway',
+    'pl': 'Polish_Poland',
+    'pt_PT': 'Portuguese_Portugal',
+    'pt_BR': 'Portuguese_Brazil',
+    'ro_RO': 'Romanian_Romania',
+    'ru_RU': 'Russian_Russia',
+    'mi_NZ': 'Maori',
+    'sr_CS': 'Serbian (Cyrillic)_Serbia and Montenegro',
+    'sk_SK': 'Slovak_Slovakia',
+    'sl_SI': 'Slovenian_Slovenia',
+    'es_ES': 'Spanish_Spain',
+    'sv_SE': 'Swedish_Sweden',
+    'ta_IN': 'English_Australia',
+    'th_TH': 'Thai_Thailand',
+    'mi_NZ': 'Maori',
+    'tr_TR': 'Turkish_Turkey',
+    'uk_UA': 'Ukrainian_Ukraine',
+    'vi_VN': 'Vietnamese_Viet Nam',
+}
+
+
 def setlang(lang=None):
     "Set language"
     locale_dir = os.path.join(CURRENT_DIR, 'share/locale')
@@ -21,11 +89,17 @@ def setlang(lang=None):
         encoding = locale.getdefaultlocale()[1]
         if encoding == 'utf':
             encoding = 'UTF-8'
+        if encoding == 'cp1252':
+            encoding = '1252'
         try:
-            locale.setlocale(locale.LC_ALL, lang+'.'+encoding)
+            if os.name == 'nt':
+                locale.setlocale(locale.LC_ALL,
+                        _LOCALE2WIN32.get(lang, lang) + '.' + encoding)
+            else:
+                locale.setlocale(locale.LC_ALL, lang + '.' + encoding)
         except:
             logging.getLogger('translate').warn(
-                    _('Unable to set locale %s') % lang+'.'+encoding)
+                    _('Unable to set locale %s') % lang + '.' + encoding)
         lang = gettext.translation(PACKAGE, locale_dir, languages=lang,
                 fallback=True)
         lang.install(unicode=1)
