6c17ac1e7a8145f63e9ecf957a5b85d729bafaa0
Cédric Krier <ced@b2ck.com>
2008-02-04 00:15:48 +0100

    Better layout for binary like image widget

--- tryton/gui/window/view_form/view/form_gtk/binary.py
+++ tryton/gui/window/view_form/view/form_gtk/binary.py
@@ -14,22 +14,41 @@ class Binary(WidgetInterface):
     def __init__(self, window, parent, model, attrs=None):
         super(Binary, self).__init__(window, parent, model, attrs)
 
+        self.tooltips = gtk.Tooltips()
+
         self.widget = gtk.HBox(spacing=5)
         self.wid_text = gtk.Entry()
         self.wid_text.set_property('activates_default', True)
         self.widget.pack_start(self.wid_text, expand=True, fill=True)
 
-        self.but_new = gtk.Button(stock='gtk-open')
+        self.but_new = gtk.Button()
+        img_new = gtk.Image()
+        img_new.set_from_stock('gtk-open', gtk.ICON_SIZE_BUTTON)
+        self.but_new.set_image(img_new)
+        self.but_new.set_relief(gtk.RELIEF_NONE)
         self.but_new.connect('clicked', self.sig_new)
+        self.tooltips.set_tip(self.but_new, _('Set File'))
         self.widget.pack_start(self.but_new, expand=False, fill=False)
 
-        self.but_save_as = gtk.Button(stock='gtk-save-as')
+        self.but_save_as = gtk.Button()
+        img_save_as = gtk.Image()
+        img_save_as.set_from_stock('gtk-save-as', gtk.ICON_SIZE_BUTTON)
+        self.but_save_as.set_image(img_save_as)
+        self.but_save_as.set_relief(gtk.RELIEF_NONE)
         self.but_save_as.connect('clicked', self.sig_save_as)
+        self.tooltips.set_tip(self.but_save_as, _('Save As'))
         self.widget.pack_start(self.but_save_as, expand=False, fill=False)
 
-        self.but_remove = gtk.Button(stock='gtk-clear')
+        self.but_remove = gtk.Button()
+        img_remove = gtk.Image()
+        img_remove.set_from_stock('gtk-clear', gtk.ICON_SIZE_BUTTON)
+        self.but_remove.set_image(img_remove)
+        self.but_remove.set_relief(gtk.RELIEF_NONE)
         self.but_remove.connect('clicked', self.sig_remove)
+        self.tooltips.set_tip(self.but_remove, _('Clear'))
         self.widget.pack_start(self.but_remove, expand=False, fill=False)
+
+        self.tooltips.enable()
 
         self.model_field = None
 
