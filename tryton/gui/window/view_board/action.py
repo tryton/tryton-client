ad671ad8c1090eb9cea8e8a6cac1378471cd14a3
Cédric Krier <ced@b2ck.com>
2008-04-17 16:21:55 +0200

    Allow to use tree view in action tag of board

--- tryton/gui/window/view_board/action.py
+++ tryton/gui/window/view_board/action.py
@@ -6,6 +6,7 @@ import time
 import datetime
 from tryton.gui.window.win_search import WinSearch
 from tryton.action import Action as Action2
+from tryton.gui.window.view_tree.view_tree import ViewTree
 
 
 class Action(object):
@@ -13,6 +14,8 @@ class Action(object):
     def __init__(self, window, attrs=None):
         self.act_id = int(attrs['name'])
         self._window = window
+        self.screen = None
+        self.tree = None
 
         try:
             self.action = rpc.execute('object', 'execute',
@@ -60,15 +63,16 @@ class Action(object):
 
         hbox.pack_start(gtk.VSeparator(), expand=False, fill=True)
 
-        eb_search = gtk.EventBox()
-        tooltips.set_tip(eb_search, _('Search'))
-        eb_search.set_events(gtk.gdk.BUTTON_PRESS)
-        eb_search.connect('button_press_event', self._sig_search)
-        img_search = gtk.Image()
-        img_search.set_from_stock('gtk-find', gtk.ICON_SIZE_MENU)
-        img_search.set_alignment(0.5, 0.5)
-        eb_search.add(img_search)
-        hbox.pack_start(eb_search, expand=False, fill=False)
+        if self.action['view_type'] == 'form':
+            eb_search = gtk.EventBox()
+            tooltips.set_tip(eb_search, _('Search'))
+            eb_search.set_events(gtk.gdk.BUTTON_PRESS)
+            eb_search.connect('button_press_event', self._sig_search)
+            img_search = gtk.Image()
+            img_search.set_from_stock('gtk-find', gtk.ICON_SIZE_MENU)
+            img_search.set_alignment(0.5, 0.5)
+            eb_search.add(img_search)
+            hbox.pack_start(eb_search, expand=False, fill=False)
 
         eb_open = gtk.EventBox()
         tooltips.set_tip(eb_open, _('Open'))
@@ -82,40 +86,41 @@ class Action(object):
 
         hbox.pack_start(gtk.VSeparator(), expand=False, fill=True)
 
-        eb_previous = gtk.EventBox()
-        tooltips.set_tip(eb_previous, _('Previous'))
-        eb_previous.set_events(gtk.gdk.BUTTON_PRESS)
-        eb_previous.connect('button_press_event', self._sig_previous)
-        img_previous = gtk.Image()
-        img_previous.set_from_stock('gtk-go-back', gtk.ICON_SIZE_MENU)
-        img_previous.set_alignment(0.5, 0.5)
-        eb_previous.add(img_previous)
-        hbox.pack_start(eb_previous, expand=False, fill=False)
+        if self.action['view_type'] == 'form':
+            eb_previous = gtk.EventBox()
+            tooltips.set_tip(eb_previous, _('Previous'))
+            eb_previous.set_events(gtk.gdk.BUTTON_PRESS)
+            eb_previous.connect('button_press_event', self._sig_previous)
+            img_previous = gtk.Image()
+            img_previous.set_from_stock('gtk-go-back', gtk.ICON_SIZE_MENU)
+            img_previous.set_alignment(0.5, 0.5)
+            eb_previous.add(img_previous)
+            hbox.pack_start(eb_previous, expand=False, fill=False)
 
-        self.label = gtk.Label('(0,0)')
-        hbox.pack_start(self.label, expand=False, fill=False)
+            self.label = gtk.Label('(0,0)')
+            hbox.pack_start(self.label, expand=False, fill=False)
 
-        eb_next = gtk.EventBox()
-        tooltips.set_tip(eb_next, _('Next'))
-        eb_next.set_events(gtk.gdk.BUTTON_PRESS)
-        eb_next.connect('button_press_event', self._sig_next)
-        img_next = gtk.Image()
-        img_next.set_from_stock('gtk-go-forward', gtk.ICON_SIZE_MENU)
-        img_next.set_alignment(0.5, 0.5)
-        eb_next.add(img_next)
-        hbox.pack_start(eb_next, expand=False, fill=False)
+            eb_next = gtk.EventBox()
+            tooltips.set_tip(eb_next, _('Next'))
+            eb_next.set_events(gtk.gdk.BUTTON_PRESS)
+            eb_next.connect('button_press_event', self._sig_next)
+            img_next = gtk.Image()
+            img_next.set_from_stock('gtk-go-forward', gtk.ICON_SIZE_MENU)
+            img_next.set_alignment(0.5, 0.5)
+            eb_next.add(img_next)
+            hbox.pack_start(eb_next, expand=False, fill=False)
 
-        hbox.pack_start(gtk.VSeparator(), expand=False, fill=True)
+            hbox.pack_start(gtk.VSeparator(), expand=False, fill=True)
 
-        eb_switch = gtk.EventBox()
-        tooltips.set_tip(eb_switch, _('Switch'))
-        eb_switch.set_events(gtk.gdk.BUTTON_PRESS)
-        eb_switch.connect('button_press_event', self._sig_switch)
-        img_switch = gtk.Image()
-        img_switch.set_from_stock('gtk-justify-left', gtk.ICON_SIZE_MENU)
-        img_switch.set_alignment(0.5, 0.5)
-        eb_switch.add(img_switch)
-        hbox.pack_start(eb_switch, expand=False, fill=False)
+            eb_switch = gtk.EventBox()
+            tooltips.set_tip(eb_switch, _('Switch'))
+            eb_switch.set_events(gtk.gdk.BUTTON_PRESS)
+            eb_switch.connect('button_press_event', self._sig_switch)
+            img_switch = gtk.Image()
+            img_switch.set_from_stock('gtk-justify-left', gtk.ICON_SIZE_MENU)
+            img_switch.set_alignment(0.5, 0.5)
+            eb_switch.add(img_switch)
+            hbox.pack_start(eb_switch, expand=False, fill=False)
 
         alignment = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
         alignment.set_padding(8, 0, 0, 0)
@@ -132,7 +137,26 @@ class Action(object):
                 self.screen.current_view.title))
             self.screen.signal_connect(self, 'record-message', self._sig_label)
         elif self.action['view_type'] == 'tree':
-            pass #TODO
+            try:
+                view_base = rpc.execute('object', 'execute',
+                        'ir.ui.view', 'read', view_ids[0],
+                        ['model', 'type'], self.context)
+            except Exception, exception:
+                rpc.process_exception(exception, self._window)
+                raise
+            try:
+                view = rpc.execute('object', 'execute',
+                        view_base['model'], 'fields_view_get', view_ids[0],
+                        view_base['type'], self.context)
+            except Exception, exception:
+                rpc.process_exception(exception, self._window)
+                raise
+            self.tree = ViewTree(view, [], self._window, True,
+                    context=self.context)
+            alignment.add(self.tree.widget_get())
+            self.title.set_text(attrs.get('string',
+                self.tree.name))
+            self.tree.view.connect('key_press_event', self.sig_key_press)
         self.widget.set_size_request(int(attrs.get('width',-1)),
                 int(attrs.get('height', -1)))
         self.display()
@@ -181,12 +205,28 @@ class Action(object):
 
     def display(self):
         try:
-            res_id = rpc.execute('object', 'execute',
+            res_ids = rpc.execute('object', 'execute',
                     self.action['res_model'], 'search', self.domain, 0,
                     self.action.get('limit', 80))
         except Exception, exception:
             rpc.process_exception(exception, self._window)
             return False
-        self.screen.clear()
-        self.screen.load(res_id)
+        if self.screen:
+            self.screen.clear()
+            self.screen.load(res_ids)
+        elif self.tree:
+            self.tree.ids = res_ids
+            self.tree.reload()
         return True
+
+    def sig_key_press(self, widget, event):
+        if event.keyval == gtk.keysyms.Left:
+            model, paths = self.tree.view.get_selection()\
+                    .get_selected_rows()
+            for path in paths:
+                self.tree.view.collapse_row(path)
+        elif event.keyval == gtk.keysyms.Right:
+            model, paths = self.tree.view.get_selection()\
+                    .get_selected_rows()
+            for path in paths:
+                self.tree.view.expand_row(path, False)
