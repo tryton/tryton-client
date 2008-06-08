01a97aab05faf4c9ef04fe1e3507f77d779c0350
Cédric Krier <ced@b2ck.com>
2008-06-08 16:02:46 +0200

    Align left for float_time widget in tree view

--- tryton/gui/window/view_tree/parse.py
+++ tryton/gui/window/view_tree/parse.py
@@ -39,7 +39,7 @@ class Parse(object):
 
                 cell = gtk.CellRendererText()
                 cell.set_fixed_height_from_font(1)
-                if field_type in ('float', 'numeric', 'integer'):
+                if field_type in ('float', 'numeric', 'integer', 'float_time'):
                     cell.set_property('xalign', 1.0)
                 column.pack_start(cell, expand=False)
                 column.add_attribute(cell, 'text', self.pos)
