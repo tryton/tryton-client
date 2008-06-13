c2c0a59106d545cbb3a4358ddce74acbd8eb9b32
Cédric Krier <ced@b2ck.com>
2008-06-13 14:19:51 +0200

    Add py2exe in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,42 @@
 
 from distutils.core import setup
 import os
+import glob
+import sys
+
+args = {}
+if os.name == 'nt':
+    import py2exe
+    args['windows'] = [{
+        'script': os.path.join('bin', 'tryton'),
+    }]
+    args['options'] = {
+        'py2exe': {
+            'optimize': 0,
+            'bundle_files': 3,
+            'packages': [
+                'encodings',
+                'gtk',
+                'pytz',
+            ],
+            'includes': 'pango,atk,gobject,cairo,pangocairo',
+            'dll_excludes': [
+                'iconv.dll',
+                'intl.dll',
+                'libatk-1.0-0.dll',
+                'libgdk_pixbuf-2.0-0.dll',
+                'libgdk-win32-2.0-0.dll',
+                'libglib-2.0-0.dll',
+                'libgmodule-2.0-0.dll',
+                'libgobject-2.0-0.dll',
+                'libgthread-2.0-0.dll',
+                'libgtk-win32-2.0-0.dll',
+                'libpango-1.0-0.dll',
+                'libpangowin32-1.0-0.dll',
+            ],
+        }
+    }
+    args['zipfile'] = None
 
 execfile(os.path.join('tryton', 'version.py'))
 
@@ -23,18 +59,19 @@ setup(name=PACKAGE,
         'tryton.gui.window.view_form.view',
         'tryton.gui.window.view_form.view.form_gtk',
         'tryton.gui.window.view_form.view.list_gtk',
+        'tryton.gui.window.view_form.view.graph_gtk',
         'tryton.gui.window.view_form.widget_search',
         'tryton.gui.window.view_tree',
+        'tryton.gui.window.view_board',
         'tryton.plugins.translation',
         'tryton.plugins.workflow',
         'tryton.wizard',
     ],
     data_files=[
-        ('share/pixmaps', [
-            'share/pixmaps/*.png',
-            'share/pixmaps/*.svg',
-            ]),
-        ('share/tryton', ['share/tryton/tryton.glade', 'share/tryton/tipoftheday.txt']),
+        ('share/pixmaps', glob.glob('share/pixmaps/*.png') + \
+                glob.glob('share/pixmaps/*.svg')),
+        ('share/tryton', glob.glob('share/tryton/tryton.glade') + \
+                glob.glob('share/tryton/tipoftheday.txt')),
     ],
     scripts=['bin/tryton'],
     classifiers=[
@@ -51,4 +88,5 @@ setup(name=PACKAGE,
     #    'xml',
     #    'pygtk (>2.0)',
     #],
+    **args
 )
