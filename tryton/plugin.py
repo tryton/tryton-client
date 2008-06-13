ccdc6811bfbc44a3b2ddbc62acc67af927d8b7d1
Cédric Krier <ced@b2ck.com>
2008-06-13 14:17:35 +0200

    Improve plugin loader if plugins dir doesn't exist

--- tryton/plugin.py
+++ tryton/plugin.py
@@ -5,13 +5,14 @@ import imp
 PLUGINS_PATH = os.path.join(os.path.dirname(__file__), 'plugins')
 MODULES = []
 
-for plugin in os.listdir(PLUGINS_PATH):
-    module = os.path.splitext(plugin)[0]
-    try:
-        module = imp.load_module(module, *imp.find_module(module, [PLUGINS_PATH]))
-        MODULES.append(module)
-    except Exception, exception:
-        continue
+if os.path.isdir(PLUGINS_PATH):
+    for plugin in os.listdir(PLUGINS_PATH):
+        module = os.path.splitext(plugin)[0]
+        try:
+            module = imp.load_module(module, *imp.find_module(module, [PLUGINS_PATH]))
+            MODULES.append(module)
+        except Exception, exception:
+            continue
 
 def execute(datas, parent):
     result = {}
