134fc7d68c3520b9a9164fe999153c89804caca6
Cédric Krier <ced@b2ck.com>
2008-06-24 11:00:45 +0200

    Use sphinx for documentation

---
+++ doc/build.py
@@ -0,0 +1,26 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+# -*- coding: utf-8 -*-
+import sys
+import os
+from os import path
+from sphinx.util.console import nocolor
+from sphinx.application import Sphinx
+
+srcdir = confdir = path.abspath(path.normpath(path.dirname(__file__)))
+outdir = os.path.join(srcdir, 'html')
+static_dir = os.path.join(srcdir, 'static')
+doctreedir = path.join(outdir, '.doctrees')
+status = sys.stdout
+confoverrides = {}
+freshenv = True
+buildername = 'html'
+if not path.isdir(outdir):
+    os.mkdir(outdir)
+if not path.isdir(static_dir):
+    os.mkdir(static_dir)
+nocolor()
+
+app = Sphinx(srcdir, confdir, outdir, doctreedir, buildername,
+             confoverrides, status, sys.stderr, freshenv)
+app.builder.build_all()
+
---
+++ doc/index.rst
@@ -0,0 +1,26 @@
+.. Tryton documentation master file. You can adapt this file
+   completely to your liking, but it should at least contain the root
+   `toctree` directive.
+
+Welcome to Tryton's documentation!
+##################################
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   installation
+   databases
+   options
+   preferences
+   forms
+   plugins
+
+Indices and tables
+##################
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
