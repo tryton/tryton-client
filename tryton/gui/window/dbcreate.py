9f53995c39503b3e723607f84d0e9e34347f8fb0
uspallek@bart.dsl <uspallek@bart.dsl>
2008-08-03 20:59:50 +0200

    Final0.1. Removed redundant gtk default values. Adjust size of dialog, added more tooltips for server connection.

--- tryton/gui/window/dbcreate.py
+++ tryton/gui/window/dbcreate.py
@@ -23,6 +23,10 @@ class DBCreate(object):
             self.entry_adminpasswd2.set_sensitive(True)
             self.entry_server_connection.modify_text(gtk.STATE_INSENSITIVE, \
                 gtk.gdk.color_parse("#000000"))
+            self.tooltips.set_tip(self.entry_server_connection,_("This is the URL of " \
+            "the Tryton server. Use server 'localhost' and port '8070' if " \
+            "the server is installed on this computer. Click on 'Change' to " \
+            "change the address."), None)
         else:
             self.entry_serverpasswd.set_sensitive(False)
             self.entry_dbname.set_sensitive(False)
@@ -31,10 +35,18 @@ class DBCreate(object):
             self.entry_server_connection.set_editable(False)
             self.entry_server_connection.set_sensitive(False)
             self.entry_server_connection.set_text( \
-                self.entry_server_connection.get_text() + "  " \
-                + _('Can not connect to server!'))
+                self.entry_server_connection.get_text()
+                + "  " + _("No connection!"))
             self.entry_server_connection.modify_text(gtk.STATE_INSENSITIVE, \
                 gtk.gdk.color_parse("#ff0000"))
+            self.tooltips.set_tip(self.entry_server_connection, _( \
+                "Can not connect to the Tryton server!\n" \
+                "1. Try to check if the Tryton server is running.\n" \
+                "2. Find out on which address and port it is listen.\n" \
+                "3. If there is a firewall between the server and this" \
+                "client, make shure that the Tryton server address and port " \
+                "(usually 8070) are not blocked.\n" \
+                "Click on 'Change' to change the address."), None)
         return state
 
     def server_change(self, widget, parent):
@@ -104,7 +116,6 @@ class DBCreate(object):
             gobject.idle_add(_move_cursor, entry, _pos)
         entry.stop_emission("insert-text")
 
-
     def __init__(self, sig_login):
         """This method defines the complete GUI."""
         self.dialog = gtk.Dialog(
@@ -113,163 +124,14 @@ class DBCreate(object):
             flags = gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT
             | gtk.WIN_POS_CENTER_ON_PARENT,
         )
+        self.dialog.set_has_separator(False)
+        self.dialog.set_size_request(700, 301)
+        self.dialog.set_icon(TRYTON_ICON)
         # This event is needed for controlling the button_create
         self.dialog.connect("key-press-event", self.event_show_button_create)
-        tooltips = gtk.Tooltips()
-        self.dialog.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
-        self.dialog.set_has_separator(False)
-        dialog_vbox = gtk.VBox()
-        table = gtk.Table(9, 3, False)
-        table.set_border_width(3)
-        table.set_row_spacings(3)
-        table.set_col_spacings(3)
-        dialog_vbox.pack_start(table)
-        label_server_setup = gtk.Label()
-        label_server_setup.set_markup("<b>"+ _("Tryton Server Setup:")+ "</b>")
-        label_server_setup.set_justify(gtk.JUSTIFY_LEFT)
-        label_server_setup.set_alignment(0, 1)
-        label_server_setup.set_padding( 9, 5)
-        table.attach(label_server_setup, 0, 3, 0, 1, xoptions=gtk.EXPAND \
-            | gtk.FILL)
-
-        label_server = gtk.Label(_("Server:"))
-        label_server.set_alignment(1, 0.5)
-        label_server.set_padding(3, 3)
-        table.attach(label_server, 0, 1, 1, 2, xoptions=gtk.FILL)
-
-        self.entry_server_connection = gtk.Entry()
-        self.entry_server_connection.set_sensitive(False)
-
-        self.entry_server_connection.unset_flags(gtk.CAN_FOCUS)
-        self.entry_server_connection.set_editable(False)
-        self.entry_server_connection.set_text(_("http://localhost:8070"))
-        self.entry_server_connection.set_width_chars(16)
-        table.attach(self.entry_server_connection, 1, 2, 1, 2)
-        tooltips.set_tip(self.entry_server_connection, _("This is the URL of " \
-            "the Tryton server. Use server 'localhost' and port '8070' if " \
-            "the server is installed on this computer. Click on 'Change' to " \
-            "change the address."), None)
-
-        self.button_server_change = gtk.Button(_("C_hange"), stock=None, 
-             use_underline=True)
-        img_button_server_change = gtk.Image()
-        img_button_server_change.set_from_stock('tryton-preferences-system', \
-            gtk.ICON_SIZE_BUTTON)
-        self.button_server_change.set_image(img_button_server_change)
-        table.attach(self.button_server_change, 2, 3, 1, 2, xoptions=gtk.FILL)
-        tooltips.set_tip(self.button_server_change, _("Setup the Tryton " \
-            "server connection..."), None)
-
-        label_serverpasswd = gtk.Label(_("Server password:"))
-        label_serverpasswd.set_justify(gtk.JUSTIFY_RIGHT)
-        label_serverpasswd.set_alignment(1, 0.5)
-        label_serverpasswd.set_padding( 3, 3)
-        table.attach(label_serverpasswd, 0, 1, 2, 3)
-
-        self.entry_serverpasswd = gtk.Entry()
-        self.entry_serverpasswd.set_max_length(16)
-        self.entry_serverpasswd.set_visibility(False)
-        self.entry_serverpasswd.set_activates_default(True)
-
-        self.entry_serverpasswd.set_width_chars(16)
-        table.attach(self.entry_serverpasswd, 1, 3, 2, 3, yoptions=False,
-            xoptions=gtk.EXPAND | gtk.FILL)
-        tooltips.set_tip(self.entry_serverpasswd, _("This is the " \
-            "password for Tryton administration. It doesn't belong to a " \
-            "Tryton user. This password is usually defined in the trytond" \
-            "configuration."), None)
-        self.entry_serverpasswd.connect("key-press-event", \
-            self.event_passwd_clear)
-
-        hseparator = gtk.HSeparator()
-        table.attach(hseparator, 0, 3, 3, 4, yoptions=False,
-            xoptions=gtk.EXPAND | gtk.FILL)
-
-        label_dbname = gtk.Label()
-        label_dbname.set_markup("<b>" + _("New database setup:")  + "</b>")
-        label_dbname.set_justify(gtk.JUSTIFY_LEFT)
-        label_dbname.set_alignment(0, 1)
-        label_dbname.set_padding( 9, 5)
-        table.attach(label_dbname, 0, 3, 4, 5, xoptions=gtk.EXPAND \
-            | gtk.FILL)
-
-        label_dbname = gtk.Label(_("Name:"))
-        label_dbname.set_justify(gtk.JUSTIFY_RIGHT)
-        label_dbname.set_padding( 3, 3)
-        label_dbname.set_alignment(1, 0.5)
-        table.attach(label_dbname, 0, 1, 5, 6, \
-            xoptions=gtk.FILL)
-
-        self.entry_dbname = gtk.Entry()
-        self.entry_dbname.set_max_length(63)
-        self.entry_dbname.set_width_chars(63)
-        self.entry_dbname.set_activates_default(True)
-        table.attach(self.entry_dbname, 1, 3, 5, 6, yoptions=False,
-                    xoptions=gtk.EXPAND | gtk.FILL)
-        tooltips.set_tip(self.entry_dbname, _("Choose the name of the new " \
-            "database.\n" \
-            "Allowed characters are alphanumerical or _ (underscore)\n" \
-            "You need to avoid all accents, space or special characters! " \
-            "Example: tryton"), None)
-        handlerid = self.entry_dbname.connect("insert-text", \
-            self.entry_insert_text)
-        self.entry_dbname.set_data('handlerid', handlerid)
-
-        label_language = gtk.Label(_("Default language:"))
-        label_language.set_justify(gtk.JUSTIFY_RIGHT)
-        label_language.set_alignment(1, 0.5)
-        label_language.set_padding( 3, 3)
-        table.attach(label_language, 0, 1, 6, 7, xoptions=gtk.FILL)
-        eventbox_language = gtk.EventBox()
-
-        self.combo_language = gtk.combo_box_new_text()
-        eventbox_language.add(self.combo_language)
-        table.attach(eventbox_language, 1, 3, 6, 7, \
-            xoptions=gtk.EXPAND | gtk.FILL)
-        tooltips.set_tip(eventbox_language, _("Choose the default " \
-            "language that will be installed for this database. You will " \
-            "be able to install new languages after installation through " \
-            "the administration menu."), None)
-
-        label_adminpasswd = gtk.Label(_("Admin password:"))
-        label_adminpasswd.set_justify(gtk.JUSTIFY_RIGHT)
-        label_adminpasswd.set_padding( 3, 3)
-        label_adminpasswd.set_alignment(1, 0.5)
-        table.attach(label_adminpasswd, 0, 1, 7, 8, xoptions=gtk.FILL)
-
-        self.entry_adminpasswd = gtk.Entry()
-        self.entry_adminpasswd.set_visibility(False)
-        self.entry_adminpasswd.set_activates_default(True)
-        tooltips.set_tip(self.entry_adminpasswd, _("Choose a password for " \
-            "the admin user of the new database. With these credentials you " \
-            "are later able to login into the database:\n" \
-            "User name: admin\n" \
-            "Password: <The password you set here>"), None)
-        table.attach(self.entry_adminpasswd, 1, 3, 7, 8, \
-            xoptions=gtk.EXPAND | gtk.FILL)
-        self.entry_adminpasswd.connect("key-press-event", \
-            self.event_passwd_clear)
-
-        label_adminpasswd2 = gtk.Label(_("Confirm admin password:"))
-        label_adminpasswd2.set_justify(gtk.JUSTIFY_RIGHT)
-        label_adminpasswd2.set_padding( 3, 3)
-        label_adminpasswd2.set_alignment(1, 0.5)
-        table.attach(label_adminpasswd2, 0, 1, 8, 9, \
-            xoptions=gtk.FILL)
-
-        self.entry_adminpasswd2 = gtk.Entry()
-        self.entry_adminpasswd2.set_visibility(False)
-        self.entry_adminpasswd2.set_activates_default(True)
-        tooltips.set_tip(self.entry_adminpasswd2, _("Type the Admin " \
-            "password again"), None)
-        table.attach(self.entry_adminpasswd2, 1, 3, 8, 9, \
-            xoptions=gtk.EXPAND | gtk.FILL)
-        self.entry_adminpasswd2.connect("key-press-event", \
-            self.event_passwd_clear)
-
+        self.tooltips = gtk.Tooltips()
         self.dialog.add_button("gtk-cancel", \
             gtk.RESPONSE_CANCEL)
-
         self.button_create = gtk.Button(_('C_reate'))
         self.button_create.set_flags(gtk.CAN_DEFAULT)
         self.button_create.set_flags(gtk.HAS_DEFAULT)
@@ -277,15 +139,139 @@ class DBCreate(object):
         img_connect = gtk.Image()
         img_connect.set_from_stock('tryton-new', gtk.ICON_SIZE_BUTTON)
         self.button_create.set_image(img_connect)
-
-        tooltips.set_tip(self.button_create, _('Create the new Tryton ' \
+        self.tooltips.set_tip(self.button_create, _('Create the new Tryton ' \
             'database.'))
         self.dialog.add_action_widget(self.button_create, gtk.RESPONSE_OK)
         self.dialog.set_default_response(gtk.RESPONSE_OK)
 
+        dialog_vbox = gtk.VBox()
+        table = gtk.Table(9, 3, False)
+        table.set_border_width(10)
+        table.set_row_spacings(3)
+        table.set_col_spacings(3)
+        dialog_vbox.pack_start(table)
+
+        label_server_setup = gtk.Label()
+        label_server_setup.set_markup("<b>"+ _("Tryton Server Setup:")+ "</b>")
+        label_server_setup.set_justify(gtk.JUSTIFY_LEFT)
+        label_server_setup.set_alignment(0, 1)
+        label_server_setup.set_padding( 9, 5)
+        table.attach(label_server_setup, 0, 3, 0, 1)
+        label_server = gtk.Label(_("Server:"))
+        label_server.set_alignment(1, 0.5)
+        label_server.set_padding(3, 3)
+        table.attach(label_server, 0, 1, 1, 2)
+        self.entry_server_connection = gtk.Entry()
+        self.entry_server_connection.set_sensitive(False)
+        self.entry_server_connection.unset_flags(gtk.CAN_FOCUS)
+        self.entry_server_connection.set_editable(False)
+        self.entry_server_connection.set_width_chars(26)
+        self.entry_server_connection.set_text(_("http://localhost:8070"))
+        self.entry_server_connection.set_width_chars(16)
+        table.attach(self.entry_server_connection, 1, 2, 1, 2)
+        self.tooltips.set_tip(self.entry_server_connection, _("This is the URL of " \
+            "the Tryton server. Use server 'localhost' and port '8070' if " \
+            "the server is installed on this computer. Click on 'Change' to " \
+            "change the address."), None)
+        self.button_server_change = gtk.Button(_("C_hange"), stock=None, 
+             use_underline=True)
+        img_button_server_change = gtk.Image()
+        img_button_server_change.set_from_stock('tryton-preferences-system', \
+            gtk.ICON_SIZE_BUTTON)
+        self.button_server_change.set_image(img_button_server_change)
+        table.attach(self.button_server_change, 2, 3, 1, 2, yoptions=False, xoptions=gtk.FILL)
+        self.tooltips.set_tip(self.button_server_change, _("Setup the Tryton " \
+            "server connection..."), None)
+        label_serverpasswd = gtk.Label(_("Server password:"))
+        label_serverpasswd.set_justify(gtk.JUSTIFY_RIGHT)
+        label_serverpasswd.set_alignment(1, 0.5)
+        label_serverpasswd.set_padding( 3, 3)
+        table.attach(label_serverpasswd, 0, 1, 2, 3)
+        self.entry_serverpasswd = gtk.Entry()
+        self.entry_serverpasswd.set_max_length(16)
+        self.entry_serverpasswd.set_visibility(False)
+        self.entry_serverpasswd.set_activates_default(True)
+        self.entry_serverpasswd.set_width_chars(16)
+        table.attach(self.entry_serverpasswd, 1, 3, 2, 3)
+        self.tooltips.set_tip(self.entry_serverpasswd, _("This is the " \
+            "password for Tryton administration. It doesn't belong to a " \
+            "Tryton user. This password is usually defined in the trytond" \
+            "configuration."), None)
+        self.entry_serverpasswd.connect("key-press-event", \
+            self.event_passwd_clear)
+
+        hseparator = gtk.HSeparator()
+        table.attach(hseparator, 0, 3, 3, 4)
+
+        label_dbname = gtk.Label()
+        label_dbname.set_markup("<b>" + _("New database setup:")  + "</b>")
+        label_dbname.set_justify(gtk.JUSTIFY_LEFT)
+        label_dbname.set_alignment(0, 1)
+        label_dbname.set_padding( 9, 5)
+        table.attach(label_dbname, 0, 3, 4, 5)
+        label_dbname = gtk.Label(_("Name:"))
+        label_dbname.set_justify(gtk.JUSTIFY_RIGHT)
+        label_dbname.set_padding( 3, 3)
+        label_dbname.set_alignment(1, 0.5)
+        table.attach(label_dbname, 0, 1, 5, 6)
+        self.entry_dbname = gtk.Entry()
+        self.entry_dbname.set_max_length(63)
+        self.entry_dbname.set_width_chars(16)
+        self.entry_dbname.set_activates_default(True)
+        table.attach(self.entry_dbname, 1, 3, 5, 6)
+        self.tooltips.set_tip(self.entry_dbname, _("Choose the name of the new " \
+            "database.\n" \
+            "Allowed characters are alphanumerical or _ (underscore)\n" \
+            "You need to avoid all accents, space or special characters! " \
+            "Example: tryton"), None)
+        handlerid = self.entry_dbname.connect("insert-text", \
+            self.entry_insert_text)
+        self.entry_dbname.set_data('handlerid', handlerid)
+        label_language = gtk.Label(_("Default language:"))
+        label_language.set_justify(gtk.JUSTIFY_RIGHT)
+        label_language.set_alignment(1, 0.5)
+        label_language.set_padding( 3, 3)
+        table.attach(label_language, 0, 1, 6, 7)
+        eventbox_language = gtk.EventBox()
+        self.combo_language = gtk.combo_box_new_text()
+        eventbox_language.add(self.combo_language)
+        table.attach(eventbox_language, 1, 3, 6, 7)
+        self.tooltips.set_tip(eventbox_language, _("Choose the default " \
+            "language that will be installed for this database. You will " \
+            "be able to install new languages after installation through " \
+            "the administration menu."), None)
+        label_adminpasswd = gtk.Label(_("Admin password:"))
+        label_adminpasswd.set_justify(gtk.JUSTIFY_RIGHT)
+        label_adminpasswd.set_padding( 3, 3)
+        label_adminpasswd.set_alignment(1, 0.5)
+        table.attach(label_adminpasswd, 0, 1, 7, 8)
+        self.entry_adminpasswd = gtk.Entry()
+        self.entry_adminpasswd.set_visibility(False)
+        self.entry_adminpasswd.set_activates_default(True)
+        self.tooltips.set_tip(self.entry_adminpasswd, _("Choose a password for " \
+            "the admin user of the new database. With these credentials you " \
+            "are later able to login into the database:\n" \
+            "User name: admin\n" \
+            "Password: <The password you set here>"), None)
+        table.attach(self.entry_adminpasswd, 1, 3, 7, 8)
+        self.entry_adminpasswd.connect("key-press-event", \
+            self.event_passwd_clear)
+        label_adminpasswd2 = gtk.Label(_("Confirm admin password:"))
+        label_adminpasswd2.set_justify(gtk.JUSTIFY_RIGHT)
+        label_adminpasswd2.set_padding( 3, 3)
+        label_adminpasswd2.set_alignment(1, 0.5)
+        table.attach(label_adminpasswd2, 0, 1, 8, 9)
+        self.entry_adminpasswd2 = gtk.Entry()
+        self.entry_adminpasswd2.set_visibility(False)
+        self.entry_adminpasswd2.set_activates_default(True)
+        self.entry_adminpasswd2.set_width_chars(16)
+        self.tooltips.set_tip(self.entry_adminpasswd2, _("Type the Admin " \
+            "password again"), None)
+        table.attach(self.entry_adminpasswd2, 1, 3, 8, 9)
+        self.entry_adminpasswd2.connect("key-press-event", \
+            self.event_passwd_clear)
         self.entry_serverpasswd.grab_focus()
         self.dialog.vbox.pack_start(dialog_vbox)
-
         self.sig_login = sig_login
 
     def run(self, parent):
@@ -317,7 +303,6 @@ class DBCreate(object):
             url = self.entry_server_connection.get_text()
             url_m = re.match('^([\w.\-]+):(\d{1,5})', \
                 url or '')
-
             langidx = self.combo_language.get_active_iter()
             langreal = langidx \
                 and self.combo_language.get_model().get_value(langidx, 1)
