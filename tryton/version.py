22ac983d1dc9aa13cd7318426879665b4fc952a3
Cédric Krier <ced@b2ck.com>
2008-09-26 13:02:09 +0200

    Fix license

--- tryton/version.py
+++ tryton/version.py
@@ -1,6 +1,7 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 PACKAGE = "tryton"
 VERSION = "0.0.1"
-LICENSE = "GPL-2"
+LICENSE = "GPL-3"
 WEBSITE = "http://www.tryton.org/"
 
