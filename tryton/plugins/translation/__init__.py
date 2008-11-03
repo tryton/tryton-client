09adfd667421f41b0d1dee1d400b80684e1a4660
Cédric Krier <ced@b2ck.com>
2008-11-03 16:47:01 +0100

    Fix plugins for gettext import and change in Window.create

--- tryton/plugins/translation/__init__.py
+++ tryton/plugins/translation/__init__.py
@@ -1,10 +1,14 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 from tryton.gui.window import Window
+import gettext
+
+_ = gettext.gettext
 
 def translate_view(datas, parent):
     model = datas['model']
-    Window.create(False, 'ir.translation', [],
-            [('model', '=', model)], 'form',
+    Window.create(False, 'ir.translation', res_id=False,
+            domain=[('model', '=', model)], view_type='form',
             mode=['tree', 'form'], window=parent)
 
 def get_plugins(model):
--- tryton/plugins/workflow/__init__.py
+++ tryton/plugins/workflow/__init__.py
@@ -1,5 +1,9 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 from tryton.action import Action
+import gettext
+
+_ = gettext.gettext
 
 def workflow_print(datas, parent):
     datas = datas.copy()
