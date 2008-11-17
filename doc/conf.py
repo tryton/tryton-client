81ffc0970d12e8ba55be56bb9bd28b939556a28a
Cédric Krier <ced@b2ck.com>
2008-11-17 15:02:19 +0100

    Increase version number

--- doc/conf.py
+++ doc/conf.py
@@ -43,9 +43,9 @@ copyright = '2008, B2CK'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.0'
+version = '1.1'
 # The full version, including alpha/beta/rc tags.
-release = '1.0.0'
+release = '1.1.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
