ab943fd547528a59a92c8d05ffe6b28ca7d59d50
Cédric Krier <ced@b2ck.com>
2008-11-17 14:40:09 +0100

    Prepare release 1.0.0

--- doc/conf.py
+++ doc/conf.py
@@ -43,9 +43,9 @@ copyright = '2008, B2CK'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.0.1'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.0.1'
+release = '1.0.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
