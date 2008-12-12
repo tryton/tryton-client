58d7bd4d55d0b56f7f7966747dc5e9e6e5a23b70
Cédric Krier <ced@b2ck.com>
2008-12-13 00:30:56 +0100

    Fix get_state_attrs if return None for issue672

--- tryton/gui/window/view_form/model/field.py
+++ tryton/gui/window/view_form/model/field.py
@@ -56,9 +56,10 @@ class CharField(object):
 
     def validate(self, model):
         res = True
-        if bool(int(self.get_state_attrs(model).get('required', 0))):
+        if bool(int(self.get_state_attrs(model).get('required') or 0)):
             if not model.value[self.name] \
-                    and not bool(int(self.get_state_attrs(model).get('readonly', 0))):
+                    and not bool(int(self.get_state_attrs(model
+                        ).get('readonly') or 0)):
                 res = False
         self.get_state_attrs(model)['valid'] = res
         return res
