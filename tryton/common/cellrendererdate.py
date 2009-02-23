3a3164b434407466faf44c840203499d19f4e7ed
Korbinian Preisler <timitos@virtual-things.biz>
2009-02-23 21:30:50 +0100

    add missing pango import on cellrendererdate

--- tryton/common/cellrendererdate.py
+++ tryton/common/cellrendererdate.py
@@ -1,6 +1,7 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
 import gtk
 import gobject
+import pango
 from date_widget import mapping, DateEntry, compute_date
 
 
