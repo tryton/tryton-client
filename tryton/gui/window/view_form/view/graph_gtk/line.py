a6743c1baf758f53104d8064385e7d36324ce2e5
Cédric Krier <ced@b2ck.com>
2009-08-03 15:29:51 +0200

    Move cairo context to center before drawing arc

--- tryton/gui/window/view_form/view/graph_gtk/line.py
+++ tryton/gui/window/view_form/view/graph_gtk/line.py
@@ -90,6 +90,8 @@ class Line(Graph):
             if key2fill[point.yname]:
                 continue
             cr.set_source_rgb(*self.colorScheme[point.yname])
+            cr.move_to(point.x * self.area.w + self.area.x,
+                    point.y * self.area.h + self.area.y)
             cr.arc(point.x * self.area.w + self.area.x,
                     point.y * self.area.h + self.area.y,
                     3, 0, 2 * math.pi)
@@ -104,6 +106,8 @@ class Line(Graph):
             if point.highlight:
                 cr.set_line_width(2)
                 cr.set_source_rgb(*hex2rgb('#000000'))
+                cr.move_to(point.x * self.area.w + self.area.x,
+                        point.y * self.area.h + self.area.y)
                 cr.arc(point.x * self.area.w + self.area.x,
                         point.y * self.area.h + self.area.y,
                         3, 0, 2 * math.pi)
