b56f496d18f10ccb32ba1c7301d4508712f73324
Cédric Krier <ced@b2ck.com>
2009-08-28 16:18:10 +0200

    Guidelines

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,6 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
 # -*- coding: utf-8 -*-
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 #
 # Tryton documentation build configuration file, created by
 # sphinx-quickstart on Tue Jun 17 08:31:05 2008.
