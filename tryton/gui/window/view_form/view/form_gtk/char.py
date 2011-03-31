08aba1e9bd3a85db09ab41538836100022e7bce6
Nicolas Évrard <nicoe@b2ck.com>
2011-03-31 13:54:59 +0200

    removed 'populate-popup' callback from entry

--- tryton/gui/window/view_form/view/form_gtk/char.py
+++ tryton/gui/window/view_form/view/form_gtk/char.py
@@ -32,8 +32,6 @@ class Char(WidgetInterface):
             self.entry.set_property('activates_default', True)
             self.entry.set_max_length(int(attrs.get('size', 0)))
             self.entry.set_width_chars(5)
-
-            self.entry.connect('populate-popup', self._populate_popup)
             self.entry.connect('activate', self.sig_activate)
 
         self.entry.connect('focus-in-event', lambda x, y: self._focus_in())
