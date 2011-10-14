78a008dde66b309bd75d9403b5b2fac5b4d5f08a
Cédric Krier <ced@b2ck.com>
2011-10-14 09:59:40 +0200

    Remove strange focus jumb in the login dialog
    review146002

--- tryton/gui/window/dblogin.py
+++ tryton/gui/window/dblogin.py
@@ -422,8 +422,6 @@ class DBLogin(object):
         self.combo_profile.add_attribute(cell, 'sensitive', 1)
         self.combo_profile.set_model(self.profile_store)
         self.combo_profile.connect('changed', self.profile_changed)
-        self.combo_profile.connect('move-active', self.profile_move_active)
-        self.move_active = False
         self.profile_label = gtk.Label(_(u'Profile:'))
         self.profile_label.set_justify(gtk.JUSTIFY_RIGHT)
         self.profile_label.set_alignment(1, 0.5)
@@ -531,16 +529,8 @@ class DBLogin(object):
         self.entry_database.set_text(self.profiles.get(profile, 'database'))
         if username:
             self.entry_login.set_text(username)
-            focus_widget = self.entry_password
         else:
             self.entry_login.set_text('')
-            focus_widget = self.entry_login
-        if not self.move_active:
-            focus_widget.grab_focus()
-        self.move_active = False
-
-    def profile_move_active(self, combobox, scrolltype):
-        self.move_active = True
 
     def clear_profile_combo(self, entry, event):
         netloc = self.entry_host.get_text()
