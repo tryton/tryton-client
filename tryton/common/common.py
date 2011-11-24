56e78b325912fc4606cf49c3c4c1ac053abc247c
Phillip Heller <pheller@me.com>
2011-11-23 20:42:04 -0500

    Fix recognition of Mac OS X based systems
    Previously, Mac OS X was recognized during py2app invocation, as well as runtime
    by comparison of os.name with 'macos'.  The value of os.name reports as 'posix'
    under more recent version of Python built on Mac OS X.  Now, the recognition is
    instead done by comparing sys.platform with 'darwin'.
    
    issue2258
    review171004
    (transplanted from d2d9d6ec6da433a5d7df4b07e089f21536b9e4fb)

--- tryton/common/common.py
+++ tryton/common/common.py
@@ -419,8 +419,7 @@ def file_open(filename, type, print_p=False):
                 save_p.close()
                 file_p.close()
         return
-    elif os.name == 'mac' or \
-            (hasattr(os, 'uname') and os.uname()[0] == 'Darwin'):
+    elif sys.platform == 'darwin':
         pid = os.fork()
         if not pid:
             pid = os.fork()
