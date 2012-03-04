9d2cc31762d51450f89ac0aee68a57ebfb991a27
Nicolas Évrard <nicoe@b2ck.com>
2012-03-04 16:02:51 +0100

    remove options settable from gtk
    review221001

--- tryton/gui/main.py
+++ tryton/gui/main.py
@@ -174,15 +174,11 @@ class Main(object):
         # Due to a bug in old version of pyGTk gtk-button-images can
         # not be set when there is no buttons
         gtk.Button()
-        for setting, value in (
-                ('gtk-button-images', True),
-                ('gtk-can-change-accels',
-                    CONFIG['client.can_change_accelerators']),
-                ('gtk-keynav-cursor-only', True)):
-            try:
-                settings.set_property(setting, value)
-            except TypeError:
-                pass
+        try:
+            settings.set_property('gtk-can-change-accels',
+                CONFIG['client.can_change_accelerators'])
+        except TypeError:
+            pass
 
         self.sig_statusbar_show()
 
