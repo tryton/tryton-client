90cf81b40e7a53bfe78bf7682a181e0aa8b38769
Mathias Behrle <mbehrle@m9s.biz>
2012-05-07 11:08:52 +0200

    Increase version number

--- tryton/version.py
+++ tryton/version.py
@@ -1,7 +1,7 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 PACKAGE = "tryton"
-VERSION = "2.0.4"
+VERSION = "2.0.5"
 LICENSE = "GPL-3"
 WEBSITE = "http://www.tryton.org/"
 
