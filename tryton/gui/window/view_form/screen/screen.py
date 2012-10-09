29f20affdb0925d5ddfb8c1f36f4b7febf2e2144
Cédric Krier <ced@b2ck.com>
2012-10-09 17:18:35 +0200

    Fix next/previous in form view when there is no current record
    issue2788
    review548003
    (transplanted from bd5d0d004c19cd4fe33f3263b065d3fdba630130)

--- tryton/gui/window/view_form/screen/screen.py
+++ tryton/gui/window/view_form/screen/screen.py
@@ -682,7 +682,9 @@ class Screen(SignalEvent):
             store = view.store
             iter_ = store.get_iter(end)
             self.current_record = store.get_value(iter_, 0)
-        elif view.view_type == 'form' and self.current_record.group:
+        elif (view.view_type == 'form'
+                and self.current_record
+                and self.current_record.group):
             group = self.current_record.group
             record = self.current_record
             while group:
@@ -724,7 +726,9 @@ class Screen(SignalEvent):
             store = view.store
             iter_ = store.get_iter(start)
             self.current_record = store.get_value(iter_, 0)
-        elif view.view_type == 'form' and self.current_record.group:
+        elif (view.view_type == 'form'
+                and self.current_record
+                and self.current_record.group):
             group = self.current_record.group
             record = self.current_record
             idx = group.index(record) - 1
