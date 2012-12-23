00ed80109d09f2f041c7d6f86184b482ece3b31e
Cédric Krier <ced@b2ck.com>
2012-12-23 16:57:00 +0100

    Increase version number

--- tryton/version.py
+++ tryton/version.py
@@ -1,7 +1,7 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 PACKAGE = "tryton"
-VERSION = "2.2.5"
+VERSION = "2.2.6"
 LICENSE = "GPL-3"
 WEBSITE = "http://www.tryton.org/"
 
