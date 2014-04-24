302b5913d91e9950b926b515f46d707f68ecbc31
Cédric Krier <ced@b2ck.com>
2014-04-24 18:04:47 +0200

    Convert only non-empty datetime from client to UTC
    issue3866
    review5261002
    (grafted from 20c4b6c559e5f7f20405dc39d99f3e61c6c6bab5)
    
    --HG--
    branch : 2.4

--- tryton/gui/window/view_form/model/field.py
+++ tryton/gui/window/view_form/model/field.py
@@ -217,7 +217,8 @@ class DateTimeField(CharField):
                         date_format() + ' ' + HM_FORMAT)[:6])
             except ValueError:
                 value = self._default
-        value = common.untimezoned_date(value)
+        if value:
+            value = common.untimezoned_date(value)
         super(DateTimeField, self).set_client(record, value,
             force_change=force_change)
 
