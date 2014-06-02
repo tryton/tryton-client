28fc0e5d5008871f5a8bc5513c8f4454fe4cd299
Guillem Barba <guillem@nan-tic.com>
2014-06-02 17:23:22 +0200

    Detect correctly when to parse value from DateTime, Date or Time field
    issue3957
    review12341002
    (grafted from 50820068cef9807e8775ae8e2768f7ab80395d5f)
    
    --HG--
    branch : 2.8

--- tryton/gui/window/view_form/model/field.py
+++ tryton/gui/window/view_form/model/field.py
@@ -196,7 +196,7 @@ class DateTimeField(CharField):
     _default = None
 
     def set_client(self, record, value, force_change=False):
-        if not isinstance(value, datetime.datetime) and value is not None:
+        if isinstance(value, basestring):
             try:
                 value = datetime.datetime(*time.strptime(value,
                         date_format() + ' ' + self.time_format(record))[:6])
@@ -224,7 +224,7 @@ class DateField(CharField):
     _default = None
 
     def set_client(self, record, value, force_change=False):
-        if not isinstance(value, datetime.date) and value is not None:
+        if isinstance(value, basestring):
             try:
                 value = datetime.date(*time.strptime(value,
                         date_format())[:3])
@@ -248,7 +248,7 @@ class TimeField(CharField):
     _default = None
 
     def set_client(self, record, value, force_change=False):
-        if not isinstance(value, datetime.time) and value is not None:
+        if isinstance(value, basestring):
             try:
                 value = datetime.time(*time.strptime(value,
                         self.time_format(record))[3:6])
