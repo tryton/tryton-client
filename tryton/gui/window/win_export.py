b3ba23cf121483bdc5e0c312e50bbe5638a4323d
Cédric Krier <ced@b2ck.com>
2014-07-29 12:38:38 +0200

    Ensure to concatenate export fields list to list
    Normally, export_fields should be a list as it comes from json-rpc except when
    inside neso. So in this case, it is a tuple since 2.6.
    
    issue4065
    review8491002
    (grafted from 21d68e8d9169019fcc0b964a3951bb7c337aecb0)
    
    --HG--
    branch : 2.6

--- tryton/gui/window/win_export.py
+++ tryton/gui/window/win_export.py
@@ -289,7 +289,7 @@ class WinExport(NoModal):
             return
         try:
             lines = RPCExecute('model', 'ir.export.line', 'read',
-                sum((x['export_fields'] for x in exports), []), None)
+                sum((list(x['export_fields']) for x in exports), []), None)
         except RPCException:
             return
         id2lines = {}
