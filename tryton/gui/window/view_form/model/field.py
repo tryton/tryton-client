339c7d32f9c03ceebff01c364fe1cdb9b7b8d388
Cédric Krier <ced@b2ck.com>
2014-10-06 22:29:46 +0200

    Empty dict value should always be an empty dictionary
    The client should not detect as changed a Dict field with None as value.
    
    issue4231
    review11531002

--- tryton/gui/window/view_form/model/field.py
+++ tryton/gui/window/view_form/model/field.py
@@ -884,6 +884,9 @@ class DictField(Field):
 
     _default = {}
 
+    def get(self, record):
+        return super(DictField, self).get(record) or self._default
+
     def get_client(self, record):
         return super(DictField, self).get_client(record) or self._default
 
