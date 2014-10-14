812649b0fcb7a596bb8e081988f92b61cbdafa6a
Cédric Krier <ced@b2ck.com>
2014-10-14 15:21:53 +0200

    domain_parser must escape when quoting '"'
    issue4247
    review11571002
    (grafted from 442c9b6e4a6ca1bef9ae9d17d8c24e5628df5858)
    
    --HG--
    branch : 2.8

--- tryton/common/domain_parser.py
+++ tryton/common/domain_parser.py
@@ -117,10 +117,18 @@ def quote(value):
     "Quote string if needed"
     if not isinstance(value, basestring):
         return value
+    if '"' in value:
+        value = value.replace('"', '\\"')
     for test in (':', ' ', '(', ')') + OPERATORS:
         if test in value:
             return '"%s"' % value
     return value
+
+
+def test_quote():
+    assert quote('test') == 'test'
+    assert quote('foo bar') == '"foo bar"'
+    assert quote('"foo"') == '\\\"foo\\\"'
 
 
 def ending_clause(domain, deep=0):
@@ -1114,6 +1122,9 @@ def test_group():
     assert rlist(dom.group(udlex(u'Name: = Name: Doe'))) == [
         ('Name', '=', None),
         ('Name', None, 'Doe'),
+        ]
+    assert rlist(dom.group(udlex(u'Name: \\"foo\\"'))) == [
+        ('Name', None, '"foo"'),
         ]
 
 
