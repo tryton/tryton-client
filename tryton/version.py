56ac04f4614acf3bd9679b80a0aa23ba8cd099d5
Cédric Krier <ced@b2ck.com>
2014-10-20 14:30:58 +0200

    Prepare release 3.4.0

--- tryton/version.py
+++ tryton/version.py
@@ -1,6 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 PACKAGE = "tryton"
-VERSION = "3.3.0"
+VERSION = "3.4.0"
 LICENSE = "GPL-3"
 WEBSITE = "http://www.tryton.org/"
