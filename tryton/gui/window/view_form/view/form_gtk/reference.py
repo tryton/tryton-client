5832d140388e74a449301b5443b30f7dfc3336ac
Cédric Krier <ced@b2ck.com>
2015-08-14 18:15:20 +0200

    Use PopdownMixin methods to set value on Reference widget
    issue4898
    review23341002
    (grafted from 739121273b3b8346625559bea1415812a2b7230b)
    
    --HG--
    branch : 3.0

--- tryton/gui/window/view_form/view/form_gtk/reference.py
+++ tryton/gui/window/view_form/view/form_gtk/reference.py
@@ -116,15 +116,13 @@ class Reference(Many2One, SelectionMixin, PopdownMixin):
         else:
             model, value = None, None
         super(Reference, self).set_text(value)
-        child = self.widget_combo.get_child()
-        reverse_selection = dict((v, k)
-            for k, v in self._selection.iteritems())
-        if model:
-            child.set_text(reverse_selection[model])
-            child.set_position(len(reverse_selection[model]))
-        else:
-            child.set_text('')
-            child.set_position(0)
+        self.widget_combo.handler_block_by_func(self.sig_changed_combo)
+        if not self.set_popdown_value(self.widget_combo, model):
+            text = self.get_inactive_selection(model)
+            self.set_popdown(
+                self.selection[:] + [(model, text)], self.widget_combo)
+            self.set_popdown_value(self.widget_combo, value)
+        self.widget_combo.handler_unblock_by_func(self.sig_changed_combo)
 
     def display(self, record, field):
         self.update_selection(record, field)
