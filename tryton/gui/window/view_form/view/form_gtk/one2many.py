ea3d9bff10b9863894b85e5d2e9396a5875a9dbc
Cédric Krier <ced@b2ck.com>
2015-11-02 11:30:34 +0100

    Always synchronise One2Many screen attributes
    The readonly, domain and size should always be set on display even if the group
    did not changed.
    
    issue5094
    review24561002

--- tryton/gui/window/view_form/view/form_gtk/one2many.py
+++ tryton/gui/window/view_form/view/form_gtk/one2many.py
@@ -501,18 +501,17 @@ class One2Many(Widget):
             if (self.screen.current_view.view_type == 'tree') \
                     and self.screen.current_view.editable:
                 self.screen.current_record = None
-            readonly = False
-            domain = []
-            size_limit = None
-            if record:
-                readonly = field.get_state_attrs(record).get('readonly', False)
-                domain = field.domain_get(record)
-                size_limit = record.expr_eval(self.attrs.get('size'))
-            if self.screen.domain != domain:
-                self.screen.domain = domain
-            if not self.screen.group.readonly and readonly:
-                self.screen.group.readonly = readonly
-            self.screen.size_limit = size_limit
+        readonly = False
+        domain = []
+        size_limit = None
+        if record:
+            readonly = field.get_state_attrs(record).get('readonly', False)
+            domain = field.domain_get(record)
+            size_limit = record.expr_eval(self.attrs.get('size'))
+        if self.screen.domain != domain:
+            self.screen.domain = domain
+        self.screen.group.readonly = readonly
+        self.screen.size_limit = size_limit
         self.screen.display()
         return True
 
