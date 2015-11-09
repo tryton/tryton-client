e4ae9b4b5d7c8ca15f13012ee6a4255583ba44d7
Cédric Krier <ced@b2ck.com>
2015-11-09 23:29:21 +0100

    Increase version number
    --HG--
    branch : 3.2

--- tryton/version.py
+++ tryton/version.py
@@ -1,6 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 PACKAGE = "tryton"
-VERSION = "3.2.11"
+VERSION = "3.2.12"
 LICENSE = "GPL-3"
 WEBSITE = "http://www.tryton.org/"
