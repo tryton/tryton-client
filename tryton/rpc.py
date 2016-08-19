01d83b58a47e56f65e700ea4ecb7689e5a5c9584
Cédric Krier <ced@b2ck.com>
2016-08-19 21:31:17 +0200

    Return None on exception in server_version
    dblogin does not except to receive an exception in test_server_version.
    Raising an exception makes the dialog freeze.
    The raise was introduced probably by mistake by changeset 0eb1d501ca9d.
    
    issue5789
    review31451003

--- tryton/rpc.py
+++ tryton/rpc.py
@@ -58,7 +58,7 @@ def server_version(host, port):
         logging.getLogger(__name__).debug(repr(result))
         return result
     except (Fault, socket.error):
-        raise
+        return None
 
 
 def login(host, port, database, username, parameters, language=None):
