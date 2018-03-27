5c1a5e1af816ecaac749523962d6d8672788c757
Cédric Krier <ced@b2ck.com>
2018-03-27 10:46:16 +0200

    Disconnect focus-out callback when xxx2many widget is destroyed
    Once destroyed, the linked screen has no view to call set_value on.
    
    issue7259
    review36071002
    (grafted from b1748cac55f75bcb99825b179dd13677c85723d9)
    
    --HG--
    branch : 3.8

--- tryton/gui/window/view_form/view/form_gtk/widget.py
+++ tryton/gui/window/view_form/view/form_gtk/widget.py
@@ -81,7 +81,7 @@ class Widget(object):
             self.visible = True
             widget.show()
 
-    def _focus_out(self):
+    def _focus_out(self, *args):
         if not self.field:
             return False
         if not self.visible:
