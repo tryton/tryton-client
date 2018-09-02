ba1008f9def6ffaa1ab90666681a153f64da9fbe
Cédric Krier <ced@b2ck.com>
2018-09-02 10:55:17 +0200

    Do not load missing view in WinForm
    This simplifies the code on sao.
    
    issue7660
    review72301002

--- tryton/gui/window/win_form.py
+++ tryton/gui/window/win_form.py
@@ -32,9 +32,6 @@ class WinForm(NoModal, InfoBar):
         self.title = title
         self.prev_view = self.screen.current_view
         self.screen.screen_container.alternate_view = True
-        if view_type not in (x.view_type for x in self.screen.views) and \
-                view_type not in self.screen.view_to_load:
-            self.screen.add_view_id(None, view_type)
         self.screen.switch_view(view_type=view_type)
         if new:
             self.screen.new(rec_name=rec_name)
