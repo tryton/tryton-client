8a8e5b158b49e8531371f24e71c00e9d9c93a8e1
Cédric Krier <ced@b2ck.com>
2018-09-19 00:13:59 +0200

    Add quick return when evaluate simple expression
    issue7696
    review49571002
    (grafted from 0e24843f01b1945491fc04cc8c20bca1418f14e2)
    
    --HG--
    branch : 4.4

--- tryton/gui/window/view_form/model/record.py
+++ tryton/gui/window/view_form/model/record.py
@@ -509,6 +509,12 @@ class Record(SignalEvent):
     def expr_eval(self, expr):
         if not isinstance(expr, basestring):
             return expr
+        if not expr:
+            return
+        elif expr == '[]':
+            return []
+        elif expr == '{}':
+            return {}
         ctx = rpc.CONTEXT.copy()
         ctx['context'] = ctx.copy()
         ctx['context'].update(self.get_context())
