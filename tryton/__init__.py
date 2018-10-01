14c8d1e166406029f8e74a1940eccd18870fcd8e
Cédric Krier <ced@b2ck.com>
2018-10-01 12:31:28 +0200

    Prepare release 5.0.0

--- tryton/__init__.py
+++ tryton/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-__version__ = "4.9.0"
+__version__ = "5.0.0"
 import sys
 
 import gi
