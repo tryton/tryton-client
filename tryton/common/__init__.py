53628f5fb47d4609ebafd7c3f73171d9c95dc953
Nicolas Évrard <nicoe@b2ck.com>
2018-12-20 18:44:31 +0100

    Fix domain inversion of child_of mixing M2O and Reference fields
    Domain inversion on Reference field does not work when using the dotted
    notation with a M2O field using child_of or parent_of.
    
    To solve this we correctly compute the localization of child_of leaves when
    using the dotted notation (which renders the inverse_leaf function useless).
    But we must also ensure that the selection used by reference fields restrict
    the choice to the right models.
    
    issue7869
    review60441002
    (grafted from 0655117211707b03fc496c0e30a9ec6c7af26e4c)
    
    --HG--
    branch : 4.8

--- tryton/common/__init__.py
+++ tryton/common/__init__.py
@@ -2,7 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 from common import *
 from datetime_strftime import *
-from domain_inversion import domain_inversion, eval_domain, localize_domain, \
-        merge, inverse_leaf, filter_leaf, concat, simplify, unique_value
+from .domain_inversion import domain_inversion, eval_domain, localize_domain, \
+        merge, inverse_leaf, filter_leaf, prepare_reference_domain, \
+        extract_reference_models, concat, simplify, unique_value
 from environment import EvalEnvironment
 import timedelta
