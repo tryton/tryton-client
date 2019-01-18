35e405bc18ac7cc9e1257bbb5d6228ad00dd9ff1
Cédric Krier <ced@b2ck.com>
2019-01-18 11:17:07 +0100

    Reduce offset when no search result
    issue7966
    review60461002

--- tryton/gui/window/view_form/screen/screen.py
+++ tryton/gui/window/view_form/screen/screen.py
@@ -238,7 +238,7 @@ class Screen(SignalEvent):
 
     def search_prev(self, search_string):
         if self.limit:
-            self.offset -= self.limit
+            self.offset = max(self.offset - self.limit, 0)
         self.search_filter(search_string=search_string)
 
     def search_next(self, search_string):
@@ -271,11 +271,16 @@ class Screen(SignalEvent):
         context = self.context
         if self.screen_container.but_active.get_active():
             context['active_test'] = False
-        try:
-            ids = RPCExecute('model', self.model_name, 'search', domain,
-                self.offset, self.limit, self.order, context=context)
-        except RPCException:
-            ids = []
+        ids = []
+        while True:
+            try:
+                ids = RPCExecute('model', self.model_name, 'search', domain,
+                    self.offset, self.limit, self.order, context=context)
+            except RPCException:
+                break
+            if ids or self.offset <= 0:
+                break
+            self.offset = max(self.offset - self.limit, 0)
         if not only_ids:
             if self.limit is not None and len(ids) == self.limit:
                 try:
