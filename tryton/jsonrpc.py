e3824b0e29327eb477bdf832b4e5820358296313
Cédric Krier <ced@b2ck.com>
2019-04-15 15:56:48 +0200

    Quote database name in URL
    issue8271
    review255281002

--- tryton/jsonrpc.py
+++ tryton/jsonrpc.py
@@ -16,7 +16,7 @@ from functools import partial
 from collections import defaultdict
 from contextlib import contextmanager
 from functools import reduce
-from urllib.parse import urljoin
+from urllib.parse import urljoin, quote
 
 __all__ = ["ResponseError", "Fault", "ProtocolError", "Transport",
     "ServerProxy", "ServerPool"]
@@ -250,6 +250,7 @@ class ServerProxy(xmlrpc.client.ServerProxy):
             fingerprints=None, ca_certs=None, session=None, cache=None):
         self.__host = '%s:%s' % (host, port)
         if database:
+            database = quote(database)
             self.__handler = '/%s/' % database
         else:
             self.__handler = '/'
