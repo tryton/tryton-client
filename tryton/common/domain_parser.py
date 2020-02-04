66a371ae933db7bfeacc48ae7292bdc1ea49d05c
Cédric Krier <ced@b2ck.com>
2020-02-04 18:28:09 +0100

    Use locale context to format date in domain parser
    issue8999
    review278401002
    (grafted from a40efbdaba86fc9236ea2ba58bb0333c81bb9d97)
    
    --HG--
    branch : 5.0

--- tryton/common/domain_parser.py
+++ tryton/common/domain_parser.py
@@ -10,7 +10,8 @@ import datetime
 import io
 from collections import OrderedDict
 
-from tryton.common import untimezoned_date, timezoned_date, datetime_strftime
+from tryton.common import (
+    untimezoned_date, timezoned_date, datetime_strftime, date_format)
 from tryton.common.datetime_ import date_parse
 from tryton.common.timedelta import parse as timedelta_parse
 from tryton.common.timedelta import format as timedelta_format
@@ -271,7 +272,8 @@ def convert_value(field, value, context=None):
     def convert_datetime():
         if not value:
             return
-        format_ = context.get('date_format', '%x') + ' %X'
+        format_ = (
+            date_format(context.get('date_format')) + ' ' + time_format(field))
         try:
             dt = date_parse(value, format_)
             return untimezoned_date(dt)
@@ -281,7 +283,7 @@ def convert_value(field, value, context=None):
     def convert_date():
         if not value:
             return
-        format_ = context.get('date_format', '%x')
+        format_ = date_format(context.get('date_format'))
         try:
             return date_parse(value, format_).date()
         except (ValueError, TypeError):
@@ -525,7 +527,8 @@ def format_value(field, value, target=None, context=None):
     def format_datetime():
         if not value:
             return ''
-        format_ = context.get('date_format', '%x') + ' ' + time_format(field)
+        format_ = (
+            date_format(context.get('date_format')) + ' ' + time_format(field))
         if not isinstance(value, datetime.datetime):
             time = datetime.datetime.combine(value, datetime.time.min)
         else:
@@ -537,7 +540,7 @@ def format_value(field, value, target=None, context=None):
     def format_date():
         if not value:
             return ''
-        format_ = context.get('date_format', '%x')
+        format_ = date_format(context.get('date_format'))
         return datetime_strftime(value, format_)
 
     def format_time():
