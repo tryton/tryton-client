9fb49594390c4754330019dc40abf098b27622ab
Cédric Krier <ced@b2ck.com>
2020-10-26 21:25:47 +0100

    Returns only Button instance instead of Gtk.Button
    The Form.get_buttons must return only the instances created by <button/> and
    not the Link which are also Gtk.Button.
    
    issue9714
    review318621002
    (grafted from 90071de08bc7b1349d661903f7d0c1f3da173d85)
    
    --HG--
    branch : 5.4

--- tryton/gui/window/view_form/view/form.py
+++ tryton/gui/window/view_form/view/form.py
@@ -476,7 +476,7 @@ class ViewForm(View):
             for w in widgets)
 
     def get_buttons(self):
-        return [b for b in self.state_widgets if isinstance(b, Gtk.Button)]
+        return [b for b in self.state_widgets if isinstance(b, Button)]
 
     def reset(self):
         record = self.record
