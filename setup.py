b3cd9eb97b930af218b10f8662f4713facc4c58d
Cédric Krier <ced@b2ck.com>
2020-12-10 23:05:58 +0100

    Add support for local_version on trydevpi.tryton.org
    issue9928

--- setup.py
+++ setup.py
@@ -74,9 +74,11 @@ if minor_version % 2:
     download_url = 'hg+http://hg.tryton.org/%s#egg=%s-%s' % (
         name, name, version)
 local_version = []
-for build in ['CI_BUILD_NUMBER', 'CI_JOB_NUMBER', 'CI_JOB_ID']:
-    if os.environ.get(build):
-        local_version.append(os.environ[build])
+if os.environ.get('CI_JOB_ID'):
+    local_version.append(os.environ['CI_JOB_ID'])
+else:
+    for build in ['CI_BUILD_NUMBER', 'CI_JOB_NUMBER']:
+        local_version.append(os.environ.get(build, ''))
 if local_version:
     version += '+' + '.'.join(local_version)
 
