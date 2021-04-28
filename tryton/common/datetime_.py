960dd486764591619fee08bdc5dfaa2f815eaae8
Cédric Krier <ced@b2ck.com>
2021-04-28 22:26:52 +0200

    Ensure zero padding for year
    It is needed to parse it back as a full year and not a year in current
    century.
    
    issue10343
    review346051003
    (grafted from d77748324d8d7fe285377f94605b9ad24ad8116d)
    
    --HG--
    branch : 5.8

--- tryton/common/datetime_.py
+++ tryton/common/datetime_.py
@@ -15,6 +15,14 @@ __all__ = ['Date', 'CellRendererDate', 'Time', 'CellRendererTime', 'DateTime']
 _ = gettext.gettext
 
 
+def _fix_format(format_):
+    if '%Y' in format_:
+        if (datetime.date.min.strftime('%Y') != '0001'
+                and datetime.date.min.strftime('%4Y') == '0001'):
+            format_ = format_.replace('%Y', '%4Y')
+    return format_
+
+
 def date_parse(text, format_='%x'):
     try:
         return datetime.datetime.strptime(text, format_)
@@ -191,7 +199,7 @@ class Date(Gtk.Entry):
             self.update_label()
             self.emit('date-changed')
         elif prop.name == 'format':
-            self.__format = value
+            self.__format = _fix_format(value)
             self.update_label()
 
     def do_get_property(self, prop):
@@ -222,7 +230,7 @@ class CellRendererDate(Gtk.CellRendererText):
 
     def do_set_property(self, prop, value):
         if prop.name == 'format':
-            self.__format = value
+            self.__format = _fix_format(value)
             return
         Gtk.CellRendererText.set_property(self, prop, value)
 
@@ -362,7 +370,7 @@ class Time(Gtk.ComboBox):
             self.update_label()
             self.emit('time-changed')
         elif prop.name == 'format':
-            self.__format = value
+            self.__format = _fix_format(value)
             self.update_label()
             self.update_model()
 
@@ -394,7 +402,7 @@ class CellRendererTime(Gtk.CellRendererCombo):
 
     def do_set_property(self, prop, value):
         if prop.name == 'format':
-            self.__format = value
+            self.__format = _fix_format(value)
             return
         Gtk.CellRendererText.set_property(self, prop, value)
 
