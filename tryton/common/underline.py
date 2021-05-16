2b77238fa83d930ebe1744a385c3f3f7366f38e2
Cédric Krier <ced@b2ck.com>
2021-05-16 18:00:27 +0200

    Set underline on the first hardware keycode
    issue10306
    review338161002

--- tryton/common/underline.py
+++ tryton/common/underline.py
@@ -1,7 +1,22 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+from gi.repository import Gdk
+
+
+def find_hardware_keycode(string):
+    keymap = Gdk.Keymap.get_for_display(Gdk.Display.get_default())
+    for i, c in enumerate(string):
+        found, keys = keymap.get_entries_for_keyval(
+            Gdk.unicode_to_keyval(ord(c)))
+        if found:
+            return i
+    return -1
 
 
 def set_underline(label):
     "Set underscore for mnemonic accelerator"
-    return '_' + label.replace('_', '__')
+    label = label.replace('_', '__')
+    position = find_hardware_keycode(label)
+    if position >= 0:
+        label = label[:position] + '_' + label[position:]
+    return label
