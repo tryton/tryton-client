1d98ca07aff0a5580f6b4e4c4c74d5def88f45a3
Cédric Krier <ced@b2ck.com>
2021-08-06 09:37:02 +0200

    Add frame around image widget
    issue10563
    review342691002

--- tryton/gui/window/view_form/view/form_gtk/image.py
+++ tryton/gui/window/view_form/view/form_gtk/image.py
@@ -21,7 +21,11 @@ class Image(BinaryMixin, Widget):
         self.height = int(attrs.get('height', 100))
         self.width = int(attrs.get('width', 300))
 
-        self.widget = Gtk.VBox(spacing=3)
+        self.widget = Gtk.Frame()
+        self.widget.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
+        self.widget.get_accessible().set_name(attrs.get('string', ''))
+        vbox = Gtk.VBox(spacing=3, margin=5)
+        self.widget.add(vbox)
         self.event = Gtk.EventBox()
         self.event.drag_dest_set(Gtk.DestDefaults.ALL, [
             Gtk.TargetEntry.new('text/plain', 0, 0),
@@ -33,12 +37,12 @@ class Image(BinaryMixin, Widget):
 
         self.image = Gtk.Image()
         self.event.add(self.image)
-        self.widget.pack_start(self.event, expand=True, fill=True, padding=0)
+        vbox.pack_start(self.event, expand=True, fill=True, padding=0)
 
         toolbar = self.toolbar()  # Set button attributes even if not display
         if not attrs.get('readonly'):
             toolbar.set_halign(Gtk.Align.CENTER)
-            self.widget.pack_start(
+            vbox.pack_start(
                 toolbar, expand=False, fill=False, padding=0)
 
         self._readonly = False
