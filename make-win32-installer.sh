108fc8959d341893bafdb820e3817de1f8948d37
Cédric Krier <ced@b2ck.com>
2021-12-16 22:19:38 +0100

    Install gdbus.exe on Windows
    gdbus.exe is needed to have interprocess communication to launch action.
    
    issue11024
    review366321002
    (grafted from eba089cb041a2cd86836c883fec1408c6bc61135)
    
    --HG--
    branch : 6.0

--- make-win32-installer.sh
+++ make-win32-installer.sh
@@ -6,4 +6,5 @@ bits=`python -c "import platform; print(platform.architecture()[0])"`
 rm -rf build dist
 ./setup.py compile_catalog
 ./setup-freeze.py install_exe -d dist
+cp `which gdbus.exe` dist/
 makensis -DVERSION=${version} -DSERIES=${series} -DBITS=${bits} setup.nsi
