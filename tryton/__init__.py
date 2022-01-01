7316ab97a991b7319e07b83bcdbcceb3fb755a73
Cédric Krier <ced@b2ck.com>
2022-01-01 17:25:33 +0100

    Increase version number
    --HG--
    branch : 6.2

--- tryton/__init__.py
+++ tryton/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-__version__ = "6.2.2"
+__version__ = "6.2.3"
 import gi
 import locale
 
