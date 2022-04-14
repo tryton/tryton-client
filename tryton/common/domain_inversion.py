30b9d8c2279f8164a944e5b23dc61fe719431ec4
Nicolas Évrard <nicoe@b2ck.com>
2022-04-14 10:21:54 +0200

    Remove extra operator when simplifying nested domains
    issue11406
    review390621002

--- tryton/common/domain_inversion.py
+++ tryton/common/domain_inversion.py
@@ -307,9 +307,14 @@ def simplify_nested(domain):
         domain_op = bool_operator(domain)
         for branch in domain:
             simplified_branch = simplify_nested(branch)
-            if (bool_operator(branch) == domain_op
+            if (bool_operator(simplified_branch) == domain_op
                     or len(simplified_branch) == 1):
-                simplified.extend(simplified_branch)
+                if (simplified
+                        and simplified_branch
+                        and simplified_branch[0] in ['AND', 'OR']):
+                    simplified.extend(simplified_branch[1:])
+                else:
+                    simplified.extend(simplified_branch)
             else:
                 simplified.append(simplified_branch)
         return simplified
